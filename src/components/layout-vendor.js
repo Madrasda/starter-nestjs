38b987394dfdb5e6ed81d3b189b439b376a204e9
PughalBot <77720496+PughalBot@users.noreply.github.com>
2023-03-09 13:07:38 +0530

    initial

---
+++ src/components/layout-vendor.js
@@ -0,0 +1,13 @@
+import NavClient from "./nav-client"
+import NavVendor from "./nav-vendor"
+import Footer from "./footer"
+
+export default function VendorLayout({ children }) {
+  return (
+    <>
+      <NavVendor />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
